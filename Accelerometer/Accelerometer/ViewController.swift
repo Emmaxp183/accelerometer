76e21e3978179b185fa292143c113f0f85e4d29c
Emmanuel Mensah <immaxp183@gmail.com>
2022-10-13 17:09:49 +0200

    setting up my project and adding some labels

---
+++ Accelerometer/Accelerometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by macbook on 13/10/2022.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zlabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
